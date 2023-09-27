d9b5fb5ef6ed0d70e2c35d578946c5efdfb676f1
Say0 <64585077+Sayy0@users.noreply.github.com>
2023-09-27 10:54:30 +0800

    staff dashboard page

---
+++ staff/staffDashboard.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Document Submission</title>
+		<link
+			href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+			rel="stylesheet"
+			integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+			crossorigin="anonymous"
+		/>
+	</head>
+	<body>
+		<!--=========================================================================== nav bar ===========================================================================-->
+		<nav class="navbar navbar-dark bg-dark text-light p-3">
+			<a href="./staffDashboard.php" class="h4 text-decoration-none">Staff Dashboard</a>
+            <div class="">
+                <ul class="nav-bar-nav list-group list-group-horizontal list-unstyled">
+                    <li class="mx-2 nav-item"><a href="" class="text-decoration-none text-light">Current Session</a></li>
+                    <li class="mx-2 nav-item"><a href="" class="text-decoration-none text-light">Upcoming Session</a></li>
+                </ul>
+            </div>
+			<div>
+				<a href="./staffProfile.php" class="mx-2 btn btn-primary">Profile</a>
+				<a href="../staffLogin.php" class="mx-2 btn btn-danger">Log Out</a>
+			</div>
+		</nav>
+		<!--=========================================================================== nav bar ===========================================================================-->
+		<div class="content p-3">
+		</div>
+	</body>
+</html>
