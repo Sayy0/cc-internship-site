c6a623c78672d03d042a849da8a439b2ee4bca83
Say0 <64585077+Sayy0@users.noreply.github.com>
2023-09-28 03:03:58 +0800

    debug

--- registerProcess.php
+++ registerProcess.php
@@ -35,6 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $getEmail = $conn->query($getEmail);
             $row = $getEmail->fetch_assoc();
             $studentId = $row["studentId"];
+            echo $studentId;
 
             $createInternQuery = "INSERT INTO Internship (studentId, indemnityStatus, parentsAckStatus, acceptanceLtrStatus, report1Status, report2Status, report3Status, findalReportStatus) VALUES ('$studentId', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty')";
             if($conn->query($createInternQuery) === TRUE){
