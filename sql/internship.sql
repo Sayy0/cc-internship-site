5775d7295bdaadd5c53aa9649087bebd904b4ffd
Say0 <jiantan420@yahoo.com>
2023-09-27 19:18:20 +0800

    Add sql file

--- sql/internship.sql
+++ sql/internship.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 26, 2023 at 08:00 PM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Host: localhost
+-- Generation Time: Sep 27, 2023 at 12:03 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,28 +24,83 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `student`
+-- Table structure for table `Internship`
 --
 
-CREATE TABLE `student` (
-  `studentId` int(7) NOT NULL,
-  `email` varchar(20) NOT NULL,
+CREATE TABLE `Internship` (
+  `internshipId` int(4) NOT NULL,
+  `studentId` int(4) NOT NULL,
+  `indemnityStatus` varchar(15) NOT NULL,
+  `parentsAckStatus` varchar(15) NOT NULL,
+  `acceptanceLtrStatus` varchar(15) NOT NULL,
+  `report1Status` varchar(15) NOT NULL,
+  `report2Status` varchar(15) NOT NULL,
+  `report3Status` varchar(15) NOT NULL,
+  `finalReportStatus` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Staff`
+--
+
+CREATE TABLE `Staff` (
+  `staffId` int(4) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `picPath` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Student`
+--
+
+CREATE TABLE `Student` (
+  `studentId` int(4) NOT NULL,
+  `email` varchar(50) NOT NULL,
   `studentName` varchar(50) NOT NULL,
   `phoneNo` varchar(10) NOT NULL,
   `gender` varchar(1) NOT NULL,
   `programme` varchar(3) NOT NULL,
-  `password` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `password` varchar(20) NOT NULL,
+  `picPath` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `student`
+-- Indexes for table `Internship`
 --
-ALTER TABLE `student`
+ALTER TABLE `Internship`
+  ADD PRIMARY KEY (`internshipId`);
+
+--
+-- Indexes for table `Staff`
+--
+ALTER TABLE `Staff`
+  ADD PRIMARY KEY (`staffId`);
+
+--
+-- Indexes for table `Student`
+--
+ALTER TABLE `Student`
   ADD PRIMARY KEY (`studentId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Student`
+--
+ALTER TABLE `Student`
+  MODIFY `studentId` int(4) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
